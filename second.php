f2fccb74016df899de4841783f6dc32f02ad3c46
rakdav <rakdav@inbox.ru>
2025-09-29 12:15:47 +0200

    6

---
+++ second.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>Задача №1</h1>
+<h3>Введите последовательность нулей и единиц:</h3>
+<form action="onemass.php" method="post">
+    <p>Введите первое число:<label>
+            <input type="number" name="mas1"/>
+        </label>
+    <p>Введите второе число:<label>
+            <input type="number" name="mas2"/>
+        </label>
+    <p>
+        <input type="submit" value="Отправить">
+</form>
+<h1>Задача №2</h1
+<form action="twomass.php" method="post">
+    <p>Введите количество строк:<label>
+            <input type="number" name="n"/>
+        </label>
+    <p>Введите количество столбцов:<label>
+            <input type="number" name="m"/>
+        </label>
+    <p>
+        <input type="submit" value="Отправить">
+</form>
+</body>
+</html>
